cf1a431e7cc0273143cc5743ebfd3ac51a2b4bab
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-10-08 06:03:11 -0400

    refactoring logging

--- src/SkewNet/utils/image_conversions.py
+++ src/SkewNet/utils/image_conversions.py
@@ -1,24 +1,7 @@
 import cv2
-from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 import numpy as np
-from multiprocessing import cpu_count
-import datetime
 import logging
-
-
-def setup_logging():
-    """
-    Configure the logging for the application.
-    """
-    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    log_filename = f"logs/jpeg_conversion_{timestamp}.log"
-
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(message)s",
-        handlers=[logging.FileHandler(log_filename), logging.StreamHandler()]
-    )
 
 
 def convert_png_to_jpeg(png_path, output_directory, new_name=None,
@@ -83,40 +66,3 @@ def get_images(input_folder):
     return list(input_folder.glob("*.png"))
 
 
-def process_task(task):
-    """
-    Helper function to process a single image.
-
-    Parameters
-    ----------
-    task : tuple
-        A tuple containing the image path and the output directory.
-    """
-    img, current_output_dir = task
-    convert_png_to_jpeg(img, current_output_dir)
-
-
-if __name__ == "__main__":
-    setup_logging()
-    logger = logging.getLogger(__name__)
-    root_output_dir = Path("/scratch/gpfs/RUSTOW/deskewing_datasets/images/cudl_images/jpeg_images")
-    root_output_dir.mkdir(parents=True, exist_ok=True)
-
-    images_root = Path("/scratch/gpfs/RUSTOW/deskewing_datasets/images/cudl_images/segmented_images")
-    subdirectories = [x for x in images_root.iterdir() if x.is_dir()]
-
-    tasks = []
-    for subdirectory in subdirectories:
-        logger.info(f"Processing {subdirectory}")
-        png_images = get_images(subdirectory)
-        logger.info(f"Found {len(png_images)} images")
-
-        current_output_dir = root_output_dir / subdirectory.name
-        current_output_dir.mkdir(parents=True, exist_ok=True)
-
-        tasks.extend((img, current_output_dir) for img in png_images)
-
-    with ProcessPoolExecutor(max_workers=cpu_count()) as executor:
-        list(executor.map(process_task, tasks))
-
-    logger.info("Finished converting images")
---
+++ src/SkewNet/utils/logging_utils.py
@@ -0,0 +1,33 @@
+import os
+import logging
+import datetime
+from logging.handlers import RotatingFileHandler
+
+
+def setup_logging(log_prefix, log_level=logging.INFO, log_dir="logs"):
+    """
+    Configure the logging for the application.
+
+    Parameters
+    ----------
+    log_prefix : str
+        A string prefix for the log filename.
+    log_level : int, optional
+        The log level, default is INFO.
+    log_dir : str, optional
+        The directory where logs will be stored, default is 'logs'.
+    """
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_filename = f"{log_dir}/{log_prefix}_{timestamp}.log"
+
+    logging.basicConfig(
+        level=log_level,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            RotatingFileHandler(log_filename, maxBytes=5000000, backupCount=5),
+            logging.StreamHandler()
+        ]
+    )
