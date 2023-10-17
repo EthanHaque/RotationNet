b03b617ee7f665e50507f43d8139b64948bb7996
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-10-17 19:08:56 -0400

    fixing formatting

--- src/SkewNet/synthetic_data_generation.py
+++ src/SkewNet/synthetic_data_generation.py
@@ -1,7 +1,8 @@
+import uuid
+
+import cv2
 import flip
 import numpy as np
-import uuid 
-import cv2
 
 
 def compose_document_onto_background(document_image, background_image, output_dir):
@@ -24,7 +25,9 @@ def compose_document_onto_background(document_image, background_image, output_di
     background_image = flip.utils.inv_channels(background_image)
 
     document_element = flip.transformers.Element(image=document_image, name="document")
-    background_element = flip.transformers.Element(image=background_image, name="background", objects=[document_element])
+    background_element = flip.transformers.Element(
+        image=background_image, name="background", objects=[document_element]
+    )
 
     document_image_width = document_image.shape[1]
     document_image_height = document_image.shape[0]
@@ -32,7 +35,7 @@ def compose_document_onto_background(document_image, background_image, output_di
     background_blur_strength = np.random.uniform(0.0, 1.0)
 
     transform_backgrounds = [
-        flip.transformers.data_augmentation.Rotate(mode='random', force=False, crop=True),
+        flip.transformers.data_augmentation.Rotate(mode="random", force=False, crop=True),
         flip.transformers.data_augmentation.Flip("random", force=False),
         flip.transformers.data_augmentation.RandomResize(
             mode="symmetric_w",
@@ -57,7 +60,6 @@ def compose_document_onto_background(document_image, background_image, output_di
         flip.transformers.data_augmentation.Rotate(mode="random", force=True, crop=False),
     ]
 
-
     name = uuid.uuid4()
     transform = flip.transformers.Compose(
         [
@@ -81,16 +83,18 @@ def main():
     Main function to test the synthetic data generation.
     """
     for i in range(10):
-            document_image = "/scratch/gpfs/RUSTOW/deskewing_datasets/images/cudl_images/rotated_images/MS-ADD-01611-000-00063.png"
-            background_image = "/scratch/gpfs/RUSTOW/deskewing_datasets/images/texture_ninja/wood-8683.jpg"
+        document_image = (
+            "/scratch/gpfs/RUSTOW/deskewing_datasets/images/cudl_images/rotated_images/MS-ADD-01611-000-00063.png"
+        )
+        background_image = "/scratch/gpfs/RUSTOW/deskewing_datasets/images/texture_ninja/wood-8683.jpg"
 
-            document_image = cv2.imread(document_image, cv2.IMREAD_UNCHANGED)
-            background_image = cv2.imread(background_image, cv2.IMREAD_UNCHANGED)
+        document_image = cv2.imread(document_image, cv2.IMREAD_UNCHANGED)
+        background_image = cv2.imread(background_image, cv2.IMREAD_UNCHANGED)
 
-            output_dir = "/scratch/gpfs/RUSTOW/deskewing_datasets/images/synthetic_data"
+        output_dir = "/scratch/gpfs/RUSTOW/deskewing_datasets/images/synthetic_data"
 
-            compose_document_onto_background(document_image, background_image, output_dir)
+        compose_document_onto_background(document_image, background_image, output_dir)
 
 
 if __name__ == "__main__":
-    main()    
+    main()
