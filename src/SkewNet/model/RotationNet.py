da1ba4e7a9a25970b5407b5f0156694cd509d5ce
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-10-18 04:01:46 -0400

    adding DL model

---
+++ src/SkewNet/model/RotationNet.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+from torchvision.models import mobilenet_v3_large
+
+
+class MyModel(nn.Module):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.base_model = mobilenet_v3_large(weights="DEFAULT", width_mult=1.0, reduced_tail=False, dilated=False)
+        self.dropout = nn.Dropout(0.2)
+        self.fc1 = nn.Linear(1000, 1)
+
+    def forward(self, x):
+        x = self.base_model(x)
+        x = self.dropout(x)
+        x = self.fc1(x)
+        return x
+
+
+if __name__ == '__main__':
+    model = MyModel()
+    print(model)
+    in_tensor = torch.randn(1, 3, 224, 224)
+    out_tensor = model(in_tensor)
+    print(out_tensor.shape)
