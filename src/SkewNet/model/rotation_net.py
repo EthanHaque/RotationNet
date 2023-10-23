c92a961b33f47bb36922343e7b88101b8812dcce
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-10-23 15:13:49 -0400

    Adding small test network

--- src/SkewNet/model/rotation_net.py
+++ src/SkewNet/model/rotation_net.py
@@ -35,3 +35,42 @@ class RotationNetMobileNetV3Backbone(nn.Module):
         ])
         return image_transform(x)
     
+
+class RotationNetSmallNetworkTest(nn.Module):
+    def __init__(self):
+        super(RotationNetSmallNetworkTest, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        
+        self.fc = nn.Linear(64, 1)
+        
+        self.relu = nn.ReLU()
+
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        
+        x = self.global_avg_pool(x)
+        x = x.view(-1, 64)
+        x = self.fc(x)
+
+        return x
+
+
+    def train_transform(self, x):
+        image_transform = transforms.Compose([
+            transforms.Resize((1200, 900), antialias=False),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+        ])
+        return image_transform(x)
+
+
+    def evaluation_transform(self, x):
+        image_transform = transforms.Compose([
+            transforms.Resize((1200, 900), antialias=False),
+        ])
+        return image_transform(x)
