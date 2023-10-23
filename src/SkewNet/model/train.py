8bb4694ea715eee3ddcf57bd3eb404721fd6883f
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-10-23 07:09:47 -0400

    adding validation transforms

--- src/SkewNet/model/train.py
+++ src/SkewNet/model/train.py
@@ -121,6 +121,10 @@ def main():
         "test": transforms.Compose([
             transforms.Resize((224, 224), antialias=False),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ]),
+        "validation": transforms.Compose([
+            transforms.Resize((224, 224), antialias=False),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
     }
     
