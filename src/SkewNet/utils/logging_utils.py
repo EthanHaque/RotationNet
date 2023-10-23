37b6ae8bba14c3bf2eccf0e735e5f4e39ab634a1
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-10-23 09:16:09 -0400

    injecting log_to_stdout parameter

--- src/SkewNet/utils/logging_utils.py
+++ src/SkewNet/utils/logging_utils.py
@@ -4,7 +4,7 @@ import os
 from logging.handlers import RotatingFileHandler
 
 
-def setup_logging(log_prefix, log_level=logging.INFO, log_dir="logs"):
+def setup_logging(log_prefix, log_level=logging.INFO, log_dir="logs", log_to_stdout=True):
     """
     Configure the logging for the application.
 
@@ -16,6 +16,8 @@ def setup_logging(log_prefix, log_level=logging.INFO, log_dir="logs"):
         The log level, default is INFO.
     log_dir : str, optional
         The directory where logs will be stored, default is 'logs'.
+    log_to_stdout : bool, optional
+        Whether to also log to stdout, default is True.
     """
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
@@ -23,11 +25,13 @@ def setup_logging(log_prefix, log_level=logging.INFO, log_dir="logs"):
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     log_filename = f"{log_dir}/{log_prefix}_{timestamp}.log"
 
+    handlers = [RotatingFileHandler(log_filename, maxBytes=5000000, backupCount=5)]
+    
+    if log_to_stdout:
+        handlers.append(logging.StreamHandler())
+
     logging.basicConfig(
         level=log_level,
         format="%(asctime)s [%(levelname)s] %(message)s",
-        handlers=[
-            RotatingFileHandler(log_filename, maxBytes=5000000, backupCount=5),
-            logging.StreamHandler()
-        ]
+        handlers=handlers
     )
