92285a177b80f5383d17e46f6a6f69b7ec8a6e56
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-10-23 07:25:26 -0400

    adding transforms per model

--- src/SkewNet/model/rotation_net.py
+++ src/SkewNet/model/rotation_net.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torchvision.models import mobilenet_v3_large
+import torchvision.transforms as transforms
 
 
 class RotationNetMobileNetV3Backbone(nn.Module):
@@ -18,3 +19,19 @@ class RotationNetMobileNetV3Backbone(nn.Module):
         return x
 
 
+    def train_transform(self, x):
+        image_transform = transforms.Compose([
+            transforms.Resize((224, 224), antialias=False),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+              ])
+        return image_transform(x)
+
+
+    def evaluation_transform(self, x):
+        image_transform = transforms.Compose([
+            transforms.Resize((224, 224), antialias=False),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        return image_transform(x)
+    
