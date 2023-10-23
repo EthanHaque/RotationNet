cbddb4464e4826d0684b8162139054e25751679a
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-10-23 06:58:51 -0400

    creating training class

--- src/SkewNet/model/train.py
+++ src/SkewNet/model/train.py
@@ -1,93 +1,66 @@
 import torch
 from tqdm import tqdm
-import torchvision.transforms.v2 as transforms
+import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torch import optim
 from model.rotated_images_dataset import RotatedImageDataset
-from model.rotation_net import RotationNet
-from model.train import train, test
+from model.rotation_net import RotationNetMobileNetV3Backbone
 from utils.logging_utils import setup_logging
 import logging
 
 
-def circular_mse(y_pred, y_true):
-    """Compute the circular mean squared error between two tensors.
-
-    Parameters
-    ----------
-    y_pred : torch.Tensor
-        The predictions from the model. Expected to be a tensor of shape
-        (batch_size, 1).
-    y_true : torch.Tensor
-        The ground truth values. Expected to be a tensor of shape
-        (batch_size, 1).
-
-    Returns
-    -------
-    torch.Tensor
-        The circular mean squared error between the two tensors.
-    """
-    error = torch.atan2(torch.sin(y_pred - y_true), torch.cos(y_pred - y_true))
-    return torch.mean(error ** 2)
+class Trainer:
+    def __init__(self, model, optimizer, device):
+        self.model = model
+        self.optimizer = optimizer
+        self.device = device
 
 
-def train(model, train_loder, optimizer, device, *args):
-    model.train()
+    def circular_mse(self, y_pred, y_true):
+        """Compute the circular mean squared error between two tensors.
 
-    total_loss = 0.0
-    for data, target in tqdm(train_loder, desc="Training", leave=False):
-        data, target = data.to(device), target.to(device)
+        Parameters
+        ----------
+        y_pred : torch.Tensor
+            The predictions from the model. Expected to be a tensor of shape
+            (batch_size, 1).
+        y_true : torch.Tensor
+            The ground truth values. Expected to be a tensor of shape
+            (batch_size, 1).
 
-        optimizer.zero_grad()
-        output = model(data)
-        loss = circular_mse(output, target)
-        loss.backward()
-        optimizer.step()
-
-        total_loss += loss.item()
-
-    average_loss = total_loss / len(train_loder)
-    # TODO remove print statement
-    print(f"Train loss: {average_loss}")
-    return average_loss
+        Returns
+        -------
+        torch.Tensor
+            The circular mean squared error between the two tensors.
+        """
+        error = torch.atan2(torch.sin(y_pred - y_true), torch.cos(y_pred - y_true))
+        return torch.mean(error ** 2)
 
 
-def test(model, test_loader, device, *args):
-    model.eval()
-
-    total_loss = 0.0
-    with torch.no_grad():
-        for data, target in tqdm(test_loader, desc="Testing", leave=False):
-            data, target = data.to(device), target.to(device)
-
-            output = model(data)
-            loss = circular_mse(output, target)
-
+    def train_epoch(self, train_loader):
+        self.model.train()
+        total_loss = 0.0
+        for data, target in tqdm(train_loader, desc="Training", leave=False):
+            data, target = data.to(self.device), target.to(self.device)
+            self.optimizer.zero_grad()
+            output = self.model(data)
+            loss = self.circular_mse(output, target)
+            loss.backward()
+            self.optimizer.step()
             total_loss += loss.item()
-
-    average_loss = total_loss / len(test_loader)
-    # TODO remove print statement
-    print(f"Test loss: {average_loss}")
-    return average_loss
+        return total_loss / len(train_loader)
 
 
-def validation(model, validation_loader, device, *args):
-    model.eval()
-
-    total_loss = 0.0
-    with torch.no_grad():
-        for data, target in tqdm(validation_loader, desc="Validation", leave=False):
-            data, target = data.to(device), target.to(device)
-
-            output = model(data)
-            loss = circular_mse(output, target)
-
-            total_loss += loss.item()
-
-    average_loss = total_loss / len(validation_loader)
-    # TODO remove print statement
-    print(f"Validation loss: {average_loss}")
-    return average_loss
+    def evaluate(self, loader, desc="Testing"):
+        self.model.eval()
+        total_loss = 0.0
+        with torch.no_grad():
+            for data, target in tqdm(loader, desc=desc, leave=False):
+                data, target = data.to(self.device), target.to(self.device)
+                output = self.model(data)
+                loss = self.circular_mse(output, target)
+                total_loss += loss.item()
+        return total_loss / len(loader)
 
 
 def main():
@@ -120,18 +93,21 @@ def main():
     test_dataset = RotatedImageDataset(annotations_file, img_dir, subset="test", transform=image_transforms["test"])
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=20, prefetch_factor=4)  # No need to shuffle the test dataset
 
-    model = RotationNet().to(device)
+    model = RotationNetMobileNetV3Backbone().to(device)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    trainer = Trainer(model, optimizer, device)
 
     for epoch in range(num_epochs):
         logger.info(f"Epoch {epoch+1}/{num_epochs}")
-        train_loss = train(model, train_loader, optimizer, device)
+
+        train_loss = trainer.train_epoch(train_loader)
         logger.info(f"Training loss: {train_loss:.4f}")
 
-        test_loss = test(model, test_loader, device)
+        test_loss = trainer.evaluate(test_loader, desc="Testing")
         logger.info(f"Test loss: {test_loss:.4f}")
 
-    torch.save(model.state_dict(), "/scratch/gpfs/RUSTOW/deskewing_models/rotation_net_mobilenet_v3_large_test.pth")
+    torch.save(model.state_dict(), "/path/to/save/model.pth")
     logger.info("Model saved successfully.")
 
 
