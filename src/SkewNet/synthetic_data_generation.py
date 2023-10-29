c5299f4ebe6d0eecaee49761b265e301c82db387
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-10-29 13:31:12 -0400

    removing flip dependency and doing by hand

--- src/SkewNet/synthetic_data_generation.py
+++ src/SkewNet/synthetic_data_generation.py
@@ -1,16 +1,16 @@
+import logging
+import os
+import time
 import uuid
+from multiprocessing import Pool, cpu_count
 
 import cv2
-import flip
 import numpy as np
-import os
-from multiprocessing import Pool, cpu_count
-import time
-from utils import logging_utils
-import logging
+from utils import image_utils, logging_utils
+import pandas as pd
 
 
-def compose_document_onto_background(document_image, background_image, output_images_dir, output_annotations_dir):
+def compose_document_onto_background(document_image, background_image, output_images_dir):
     """
     Composes a document image onto a background image applying a series
     of transformations to the document image and the background image.
@@ -19,93 +19,87 @@ def compose_document_onto_background(document_image, background_image, output_im
     ----------
     document_image : numpy.ndarray
         Document image to be composed onto the background image.
-
     background_image : numpy.ndarray
         Background image.
-
     output_dir : str
         Output directory where the composed image will be saved.
+
+    Returns
+    -------
+    annotation : dict
+        Dictionary containing the annotation information.
     """
     logger = logging.getLogger(__name__)
-    document_image = flip.utils.inv_channels(document_image)
-    background_image = flip.utils.inv_channels(background_image)
 
-    document_element = flip.transformers.Element(image=document_image, name="document")
-    background_element = flip.transformers.Element(
-        image=background_image, name="background", objects=[document_element]
-    )
+    initial_backgound_height, initial_backgound_width = background_image.shape[:2]
+    initial_document_height, initial_document_width = document_image.shape[:2]
 
-    document_angle = np.random.uniform(0.0, 360.0)
-    background_angle = np.random.uniform(0.0, 360.0)
-    # with probability 1/2 set background angle to 0
-    if np.random.randint(0, 2) == 0:
-        background_angle = 0.0
+    document_angle = np.random.uniform(0.0, 30.0) * np.pi / 180.0
+    background_angle = np.random.uniform(0.0, 360.0) * np.pi / 180.0
+    target_background_width = 900
+    target_background_height = 1200
 
-    rotated_document_width, rotated_document_height = flip.utils.get_size_of_rotated_bounding_box(
-        document_image.shape[1], document_image.shape[0], document_angle
-    )
-    rotated_background_width, rotated_background_height = flip.utils.get_size_of_rotated_bounding_box(
-        background_image.shape[1], background_image.shape[0], background_angle
-    )
-    rotated_background_width, rotated_background_height = flip.utils.largest_inscribed_rectangle(
-        rotated_background_width, rotated_background_height, background_angle
-    )
+    rotated_document_width, rotated_document_height = image_utils.get_size_of_rotated_image(document_image.shape[1], document_image.shape[0], document_angle)
+    rotated_background_width, rotated_background_height = image_utils.get_size_of_rotated_image(background_image.shape[1], background_image.shape[0], background_angle)
 
-    largest_document_dimension = max(rotated_document_width, rotated_document_height)
-    largest_background_dimension = max(rotated_background_width, rotated_background_height)
-    smallest_dimension = min(largest_document_dimension, largest_background_dimension)
-    # print all the dimensions
-    # print(f"rotated_document_width: {rotated_document_width:.2f}, rotated_document_height: {rotated_document_height:.2f}, rotated_background_width: {rotated_background_width:.2f}, rotated_background_height: {rotated_background_height:.2f}, largest_document_dimension: {largest_document_dimension:.2f}, largest_background_dimension: {largest_background_dimension:.2f}, smallest_dimension: {smallest_dimension:.2f}")
-    background_blur_strength = np.random.uniform(0.0, 1.0)
+    scale_down_factor = np.random.uniform(0.75, 0.95)
+    largest_document_side = max(rotated_document_width, rotated_document_height)
+    smallest_target_size = min(target_background_width, target_background_height)
+    scale = scale_down_factor * smallest_target_size / largest_document_side
 
-    transform_backgrounds = [
-        flip.transformers.data_augmentation.Rotate(mode="by_angle", angle=background_angle, force=True, crop=True),
-        flip.transformers.data_augmentation.Flip("random", force=False),
-        flip.transformers.data_augmentation.RandomResize(
-            mode="asymmetric",
-            w_max=smallest_dimension * 1.7,
-            h_max=smallest_dimension * 1.7,
-            w_min=smallest_dimension * 1.0,
-            h_min=smallest_dimension * 1.0,
-            force=True,
-        ),
-        flip.transformers.data_augmentation.Noise("gaussian_blur", value=background_blur_strength, force=False),
-    ]
+    document_image = cv2.resize(document_image, (0, 0), fx=scale, fy=scale)
+    mask = np.ones(document_image.shape, dtype=np.uint8) * 255
 
-    # Transformations to apply to the document image i.e. children of the background image
-    transform_objects = [
-        flip.transformers.data_augmentation.Rotate(mode="by_angle", angle=document_angle, force=True, crop=False),
-        flip.transformers.data_augmentation.RandomResize(
-            mode="larger",
-            w_min=smallest_dimension * 0.8,
-            w_max=smallest_dimension * 0.95,
-            h_min=smallest_dimension * 0.8,
-            h_max=smallest_dimension * 0.95,
-            force=True,
-        ),
-    ]
+    document_image = image_utils.rotate_image(document_image, document_angle)
+    mask = image_utils.rotate_image(mask, document_angle)
+
+    random_flip_direciton = np.random.randint(-1, 2)
+    background_image = image_utils.flip_image(background_image, random_flip_direciton)
+    background_image = image_utils.rotate_image(background_image, background_angle)
+    background_image = image_utils.crop_from_angle(background_image, initial_backgound_width, initial_backgound_height, -background_angle)
+
+    random_width_scale = np.random.uniform(1.0, 1.1)
+    random_height_scale = np.random.uniform(1.0, 1.1)
+
+    # subtract 1 to avoid out of bounds error
+    crop_width = int(target_background_width * random_width_scale)
+    crop_width = min(crop_width, background_image.shape[1]) - 1
+    crop_height = int(target_background_height * random_height_scale) 
+    crop_height = min(crop_height, background_image.shape[0]) - 1
+
+    random_crop_x1 = np.random.randint(0, background_image.shape[1] - crop_width)
+    random_crop_y1 = np.random.randint(0, background_image.shape[0] - crop_height)
+
+
+    background_image = image_utils.crop_image(background_image, random_crop_x1, random_crop_y1, crop_width, crop_height)
+    background_image = cv2.resize(background_image, (target_background_width, target_background_height))
+
+    superimposed_image_x = np.random.randint(0, background_image.shape[1] - document_image.shape[1])
+    superimposed_image_y = np.random.randint(0, background_image.shape[0] - document_image.shape[0])
+
+    superimposed_image = image_utils.superimpose_image_on_background(document_image, background_image, mask, superimposed_image_x, superimposed_image_y)
 
     name = uuid.uuid4()
-    transform = flip.transformers.Compose(
-        [
-            flip.transformers.ApplyToBackground(transform_backgrounds),
-            flip.transformers.ApplyToObjects(transform_objects),
-            flip.transformers.domain_randomization.ObjectsRandomPosition(
-                x_min=0, y_min=0, x_max=1, y_max=1, mode="percentage"
-            ),
-            flip.transformers.domain_randomization.Draw(),
-            flip.transformers.labeler.CreateBoundingBoxes(),
-            flip.transformers.labeler.CreateAngles(),
-            flip.transformers.io.SaveImage(output_images_dir, name),
-            flip.transformers.io.CreateJson(output_annotations_dir, name),
-        ]
-    )
+    cv2.imwrite(os.path.join(output_images_dir, f"{name}.jpg"), superimposed_image)
 
-    [background_element] = transform(background_element)
+    annotation = {
+        "image_name": f"{name}.jpg",
+        "document_angle": document_angle,
+        "background_angle": background_angle,
+        "scale": scale,
+        "random_flip_direciton": random_flip_direciton,
+        "random_width_scale": random_width_scale,
+        "random_height_scale": random_height_scale,
+        "random_crop_x1": random_crop_x1,
+        "random_crop_y1": random_crop_y1,
+        "crop_width": crop_width,
+        "crop_height": crop_height,
+        "superimposed_image_x": superimposed_image_x,
+        "superimposed_image_y": superimposed_image_y,
 
-    logger.info(f"Saved image {name} with {len(background_element.objects)} objects")
+    }
 
-    return background_element
+    return annotation
 
 
 def collect_files(directory):
@@ -128,7 +122,7 @@ def collect_files(directory):
     return [os.path.join(directory, f) for f in os.listdir(directory)]
 
 
-def process_image(image_path, background_path, output_images_dir, output_annotations_dir, index):
+def process_image(image_path, background_path, output_images_dir, index):
     """
     Composes a document image onto a background image applying a series
     of transformations to the document image and the background image.
@@ -137,24 +131,23 @@ def process_image(image_path, background_path, output_images_dir, output_annotat
     ----------
     image_path : str
         Path to the document image.
-
     background_path : str
         Path to the background image.
-
     output_images_dir : str
         Output directory where the composed image will be saved.
-
-    output_annotations_dir : str
-        Output directory where the annotations will be saved.
-
     index : int
         Index of the image to be saved.
+
+    Returns
+    -------
+    annotation : dict
+        Dictionary containing the annotation information.
     """
     logger = logging.getLogger(__name__)
     logger.info(f"Processing image {index} with path {image_path}")
 
     np.random.seed(int(time.time()) + index)
-    
+
     document_image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
     if (document_image.shape[2]) == 3:
         document_image = cv2.cvtColor(document_image, cv2.COLOR_BGR2BGRA)
@@ -163,15 +156,23 @@ def process_image(image_path, background_path, output_images_dir, output_annotat
     if (background_image.shape[2]) == 3:
         background_image = cv2.cvtColor(background_image, cv2.COLOR_BGR2BGRA)
 
-    compose_document_onto_background(document_image, background_image, output_images_dir, output_annotations_dir)
+    
+    if background_image.dtype != np.uint8:
+        logger.warning(f"Background image {background_path} has dtype {background_image.dtype}")
+        return
+
+    annotation = compose_document_onto_background(document_image, background_image, output_images_dir)
+    return annotation
 
 
 def main():
     """
     Main function to test the synthetic data generation.
     """
-    logging_utils.setup_logging("synthetic_data_generation", log_level=logging_utils.logging.INFO)
+    logging_utils.setup_logging("synthetic_data_generation", log_level=logging_utils.logging.INFO, log_to_stdout=False) 
     logger = logging.getLogger(__name__)
+
+    annotations_file = "/scratch/gpfs/eh0560/datasets/deskewing/synthetic_image_angles.csv"
 
     cudl_document_images = collect_files("/scratch/gpfs/RUSTOW/deskewing_datasets/images/cudl_images/rotated_images")
     doc_lay_net_document_images = collect_files("/scratch/gpfs/RUSTOW/deskewing_datasets/images/doc_lay_net/images")
@@ -179,17 +180,19 @@ def main():
 
     texture_ninja_background_images = collect_files("/scratch/gpfs/RUSTOW/deskewing_datasets/images/texture_ninja")
     pexels_background_images = collect_files("/scratch/gpfs/RUSTOW/deskewing_datasets/images/pexels_textures")
-    # TODO: add images with solid colors as backgrounds
+    # TODO: add images with solid colors as backgrounds and simple textures
 
-    output_images_dir = "/scratch/gpfs/RUSTOW/deskewing_datasets/images/synthetic_data"
-    output_annotations_dir = "/scratch/gpfs/RUSTOW/deskewing_datasets/images/synthetic_data_annotations"
+    output_images_dir = "/scratch/gpfs/eh0560/datasets/deskewing/synthetic_data"
+
+    os.makedirs(output_images_dir, exist_ok=True)
 
     document_images = []
 
     # for i in range(10):
     #     document_images.extend(cudl_document_images)
-    # document_images.extend(doc_lay_net_document_images)
+    document_images.extend(doc_lay_net_document_images)
     document_images.extend(publaynet_document_images)
+    # document_images = [image for image in document_images for _ in range(5)]
 
     background_images = []
     background_images.extend(texture_ninja_background_images)
@@ -200,20 +203,27 @@ def main():
 
     random_background_images = np.random.choice(background_images, len(document_images))
 
-    with Pool(cpu_count()) as p:
-        p.starmap(
+    annotations = []
+    workers = cpu_count()
+    logger.info(f"Using {workers} workers to process images")
+    with Pool(workers) as p:
+        annotations = p.starmap(
             process_image,
             zip(
                 document_images,
                 random_background_images,
                 [output_images_dir] * len(document_images),
-                [output_annotations_dir] * len(document_images),
                 range(len(document_images)),
             ),
         )
 
+    annotations = [annotation for annotation in annotations if annotation is not None]
+    annotations_df = pd.DataFrame(annotations)
+    annotations_df.to_csv(annotations_file, index=False)
+
     # for i in range(len(document_images)):
-    #     process_image(document_images[i], random_background_images[i], output_dir, i)
+    #     process_image(document_images[i], random_background_images[i], output_images_dir, i)
+
 
 if __name__ == "__main__":
     main()
