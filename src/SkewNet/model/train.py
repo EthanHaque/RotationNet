68ad583e4b5012058f9e18ef37ad6badd933a23d
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-10-31 15:07:04 -0400

    going back to manual dataloder creation

--- src/SkewNet/model/train.py
+++ src/SkewNet/model/train.py
@@ -4,14 +4,15 @@ import lightning.pytorch as pl
 import torch
 import torch.optim as optim
 
-from SkewNet.model.rotated_images_dataset import RotatedImagesModule
+from SkewNet.model.rotated_images_dataset import RotatedImageDataset
 from SkewNet.model.rotation_net import RotationNetSmallNetworkTest
+from torch.utils.data import DataLoader
+from torchvision import transforms
 
 
 class LightningRotationNet(pl.LightningModule):
     def __init__(self, model):
         super().__init__()
-        self.save_hyperparameters()
         self.model = model
 
     def mse(self, y_pred, y_true):
@@ -47,19 +48,72 @@ class LightningRotationNet(pl.LightningModule):
         optimizer = optim.Adam(self.model.parameters(), lr=0.004)
         scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
         return [optimizer], [scheduler]
+    
+
+def setup_data_loaders(annotations_file, img_dir, batch_size, num_workers, prefetch_factor, train_transform, val_transform, test_transform):
+    train_dataset = RotatedImageDataset(
+        annotations_file,
+        img_dir,
+        subset="train",
+        transform=train_transform,
+    )
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        prefetch_factor=prefetch_factor,
+    )
+
+    test_dataset = RotatedImageDataset(
+        annotations_file,
+        img_dir,
+        subset="test",
+        transform=test_transform,
+    )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        prefetch_factor=prefetch_factor,
+    )
+
+    val_dataset = RotatedImageDataset(
+        annotations_file,
+        img_dir,
+        subset="val",
+        transform=val_transform,
+    )
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        prefetch_factor=prefetch_factor,
+    )
+    return train_loader, val_loader, test_loader
+
 
 
 def main():
-    BATCH_SIZE = 48
-    NUM_EPOCHS = 3
-    NUM_WORKERS = int(os.environ["SLURM_CPUS_PER_TASK"])
-    NUM_NODES = int(os.environ["SLURM_NNODES"])
-    ALLOCATED_GPUS_PER_NODE = int(os.environ["SLURM_GPUS_ON_NODE"])
+    # BATCH_SIZE = 48
+    BATCH_SIZE = 16
+    # NUM_EPOCHS = 3
+    NUM_EPOCHS = 1
+    # NUM_WORKERS = int(os.environ["SLURM_CPUS_PER_TASK"])
+    NUM_WORKERS = 2
+    # NUM_NODES = int(os.environ["SLURM_NNODES"])
+    NUM_NODES = 1
+    # ALLOCATED_GPUS_PER_NODE = int(os.environ["SLURM_GPUS_ON_NODE"])
+    ALLOCATED_GPUS_PER_NODE = 1
 
-    # Trades off precision for performance
-    torch.set_float32_matmul_precision("medium" | "high")
 
     model = LightningRotationNet(RotationNetSmallNetworkTest())
+
+    train_transform = transforms.Compose([transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),])
+    test_transform = transforms.Compose([])
+    val_transform = transforms.Compose([])
 
     trainer = pl.Trainer(
         accelerator="gpu",
@@ -81,14 +135,21 @@ def main():
 
     img_dir = "/scratch/gpfs/eh0560/datasets/deskewing/synthetic_data"
     annotations_file = "/scratch/gpfs/eh0560/datasets/deskewing/synthetic_image_angles.csv"
-    images = RotatedImagesModule(
-        img_dir,
+    
+    train_loader, val_loader, test_loader = setup_data_loaders(
         annotations_file,
+        img_dir,
         BATCH_SIZE,
         NUM_WORKERS,
+        prefetch_factor=1,
+        train_transform=train_transform,
+        val_transform=val_transform,
+        test_transform=test_transform,
     )
 
-    trainer.fit(model, images)
+
+    trainer.fit(model, train_loader, val_loader)
+    trainer.test(model, test_loader)
 
 
 if __name__ == "__main__":
