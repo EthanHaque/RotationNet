63eae2357ea96bd5a86a19ba5f9823c99f8b2fea
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-11-01 20:04:42 -0400

    adding huge network

--- src/SkewNet/model/rotation_net.py
+++ src/SkewNet/model/rotation_net.py
@@ -105,3 +105,69 @@ class RotationNetLargeNetworkTest(nn.Module):
         x = self.fc2(x)
 
         return x
+
+
+@ModelRegistry.register("HugeTestNetwork")
+class RotationNetHugeNetworkTest(nn.Module):
+    def __init__(self):
+        super(RotationNetHugeNetworkTest, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=7, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=7, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=5, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(256)
+
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.bn6 = nn.BatchNorm2d(256)
+
+        self.conv7 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.bn7 = nn.BatchNorm2d(256)
+
+        self.conv8 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        self.bn8 = nn.BatchNorm2d(512)
+
+        self.conv9 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.bn9 = nn.BatchNorm2d(512)
+
+        self.conv10 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.bn10 = nn.BatchNorm2d(512)
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.fc1 = nn.Linear(512, 256)
+        self.dropout = nn.Dropout(p=0.5)
+
+        self.fc2 = nn.Linear(256, 1)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.bn1(self.conv1(x))))
+        x = self.pool(self.relu(self.bn2(self.conv2(x))))
+        x = self.pool(self.relu(self.bn3(self.conv3(x))))
+        x = self.pool(self.relu(self.bn4(self.conv4(x))))
+        x = self.pool(self.relu(self.bn5(self.conv5(x))))
+        x = self.pool(self.relu(self.bn6(self.conv6(x))))
+        x = self.pool(self.relu(self.bn7(self.conv7(x))))
+        x = self.pool(self.relu(self.bn8(self.conv8(x))))
+        x = self.pool(self.relu(self.bn9(self.conv9(x))))
+        x = self.pool(self.relu(self.bn10(self.conv10(x))))
+
+        x = self.global_avg_pool(x)
+        x = x.view(-1, 512)
+
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+
+        return x
