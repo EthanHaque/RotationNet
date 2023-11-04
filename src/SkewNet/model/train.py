8bd0a017302b70abe2950e21db88461b0940901b
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-11-04 14:48:31 -0400

    rewriting distributed training loop in native pytorch

--- src/SkewNet/model/train.py
+++ src/SkewNet/model/train.py
@@ -1,195 +1,293 @@
 import argparse
+import json
 import os
+from dataclasses import dataclass
 
-import pytorch_lightning as pl
 import torch
-import torch.optim as optim
-
-from SkewNet.model.rotated_images_dataset import RotatedImageDataset
-from SkewNet.model.rotation_net import ModelRegistry
-from SkewNet.model.callbacks.better_progress_bar import BetterProgressBar
+import torch.profiler
+from torch.distributed import destroy_process_group, init_process_group
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.nn.utils import clip_grad_norm_
 from torch.utils.data import DataLoader
-from torchvision import transforms
-from torchvision.utils import make_grid
+from torch.utils.data.distributed import DistributedSampler
+
+from SkewNet.model.rotated_images_dataset import DataConfig, RotatedImageDataset
+from SkewNet.model.rotation_net import ModelRegistry
 
 
-class LightningRotationNet(pl.LightningModule):
-    def __init__(self, model, learning_rate, weight_decay, *args, **kwargs):
-        super().__init__()
-        self.save_hyperparameters(ignore=["model"])
+@dataclass
+class TrainConfig:
+    evaluate: bool = True
+    start_epoch: int = 0
+    max_epochs: int = 20
+    batch_size: int = 64
+    data_loader_prefetch_factor: int = 4
+    data_loader_num_workers: int = 8
+    snapshot_dir: str = ""
+    snapshot_path: str = ""
+    resume: bool = False
+    snapshot_interval: int = 1
+    use_automatic_mixed_precision: bool = False
+    grad_norm_clip: float = 1.0
+
+
+@dataclass
+class SnapshotConfig:
+    model_state: dict
+    optimizer_state: dict
+    scheduler_state: dict
+    epoch: int
+    best_loss: float
+    best_epoch: int
+
+
+@dataclass
+class OptimizerConfig:
+    learning_rate: float
+    weight_decay: float
+
+
+@dataclass
+class SchedulerConfig:
+    step_size: int
+    gamma: float
+
+
+class Trainer:
+    def __init__(self, trainer_config, model, criterion, optimizer, scheduler, train_dataset, val_dataset=None):
+        if trainer_config.evaluate and not val_dataset:
+            raise ValueError("Validation dataset must be provided when in evaluation mode")
+        if not trainer_config.evaluate and val_dataset:
+            raise ValueError("Validation dataset provided but not in evaluation mode.")
+        if not trainer_config.snapshot_dir:
+            raise ValueError("No snapshot directory provided.")
+
+        self.config = trainer_config
+
+        self.local_rank = int(os.environ.get("LOCAL_RANK", 0))
+        self.global_rank = int(os.environ.get("RANK", 0))
+
         self.model = model
+        self.model = model.to(self.local_rank)
 
-    def mse(self, y_pred, y_true, scale=1):
-        return torch.mean((y_pred - y_true) ** 2) * scale
-    
-    def mae(self, y_pred, y_true, scale=1):
-        return torch.mean(torch.abs(y_pred - y_true)) * scale
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.scheduler = scheduler
 
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self.model(x)
-        loss = self.mse(y_hat, y)
-        self.log("train_loss", loss, sync_dist=True, prog_bar=True)
-        self.log("train_mae", self.mae(y_hat, y), sync_dist=True, prog_bar=True)
+        self.train_dataset = train_dataset
+        self.train_loader = self._prepare_distributed_dataloader(train_dataset)
+        self.val_loader = self._prepare_distributed_dataloader(val_dataset) if val_dataset else None
+
+        self.epochs_run = 0
+        self.best_loss = float("inf")
+        self.best_epoch = 0
+        self.snapshot_interval = max(self.config.snapshot_interval, 1)
+
+        if self.config.use_automatic_mixed_precision:
+            self.scaler = torch.cuda.amp.GradScaler()
+
+        if not self.config.snapshot_path:
+            self.config.snapshot_path = os.path.join(
+                self.config.snapshot_dir, self.model.__class__.__name__, "snapshot.pth"
+            )
+
+        if not os.path.exists(os.path.dirname(self.config.snapshot_path)):
+            os.makedirs(os.path.dirname(self.config.snapshot_path))
+        if self.config.resume:
+            self._load_snapshot(self.config.snapshot_path)
+
+        self.model = DDP(self.model, device_ids=[self.local_rank])
+
+    def _prepare_distributed_dataloader(self, dataset):
+        return DataLoader(
+            dataset,
+            batch_size=self.config.batch_size,
+            pin_memory=True,
+            shuffle=False,
+            num_workers=self.config.data_loader_num_workers,
+            prefetch_factor=self.config.data_loader_prefetch_factor,
+            sampler=DistributedSampler(dataset, shuffle=True),
+        )
+
+    def _load_snapshot(self, snapshot_path):
+        try:
+            snapshot = torch.load(snapshot_path)
+        except FileNotFoundError:
+            print(f"Snapshot not found at {snapshot_path}")
+            return
+
+        self.model.load_state_dict(snapshot.model_state)
+        self.optimizer.load_state_dict(snapshot.optimizer_state)
+        self.scheduler.load_state_dict(snapshot.scheduler_state)
+        self.epochs_run = snapshot.epoch
+        self.best_loss = snapshot.best_loss
+        self.best_epoch = snapshot.best_epoch
+        print(f"GPU {self.global_rank} | Loaded snapshot from {snapshot_path}")
+
+    def _save_snapshot(self, snapshot_path):
+        model = self.model.module if isinstance(self.model, DDP) else self.model
+        snapshot = SnapshotConfig(
+            model_state=model.state_dict(),
+            optimizer_state=self.optimizer.state_dict(),
+            scheduler_state=self.scheduler.state_dict(),
+            epoch=self.epochs_run,
+            best_loss=self.best_loss,
+            best_epoch=self.best_epoch,
+        )
+        torch.save(snapshot, snapshot_path)
+
+    def _training_step(self, batch, batch_idx):
+        with torch.set_grad_enabled(True), torch.amp.autocast(
+            device_type="cuda", dtype=torch.float16, enabled=self.config.use_automatic_mixed_precision
+        ):
+            x, y = batch
+            x, y = x.to(self.local_rank), y.to(self.local_rank)
+            y_hat = self.model(x)
+            loss = self.criterion(y_hat, y)
+            self.optimizer.zero_grad(set_to_none=True)
+            if self.config.use_automatic_mixed_precision:
+                self.scaler.scale(loss).backward()
+                clip_grad_norm_(self.model.parameters(), self.config.grad_norm_clip)
+                self.scaler.step(self.optimizer)
+                self.scaler.update()
+            else:
+                loss.backward()
+                clip_grad_norm_(self.model.parameters(), self.config.grad_norm_clip)
+                self.optimizer.step()
+
+            self.scheduler.step()
+            print(f"GPU {self.global_rank} EPOCH {self.epochs_run} | Batch {batch_idx} | Train Loss: {loss:.5f}")
+
         return loss
 
-    def _make_grid(self, x, y, y_hat):
-        num_images = min(x.size(0), 4)  
-        x = x[:num_images]
+    def _train(self, epoch, data_loader):
+        data_loader.sampler.set_epoch(epoch)
+        self.model.train()
+        running_loss = 0.0
+        for idx, batch in enumerate(data_loader):
+            batch_loss = self._training_step(batch, idx) 
+            running_loss += batch_loss
 
-        degrees = y_hat[:num_images] * 180 / 3.141592
-        degrees = degrees.view(-1).tolist()  
+        average_loss = running_loss / len(data_loader)
+        print(f"GPU {self.global_rank} EPOCH {self.epochs_run} | Train Loss: {average_loss:.5f}")
+        return average_loss
 
-        rotated_images = []
-        for i in range(num_images):  
-            rotated_image = transforms.functional.rotate(x[i].unsqueeze(0), angle=-degrees[i], expand=False)
-            rotated_images.append(rotated_image.squeeze(0))  # Remove the batch dimension after rotating
+    def _eval_step(self, batch, batch_idx):
+        with torch.set_grad_enabled(False):
+            x, y = batch
+            x, y = x.to(self.local_rank), y.to(self.local_rank)
+            y_hat = self.model(x)
+            loss = self.criterion(y_hat, y)
+            print(f"GPU {self.global_rank} EPOCH {self.epochs_run} | Batch {batch_idx} | Val Loss: {loss:.5f}")
+            return loss
 
-        # Create a grid of images with 2 images per row
-        grid = make_grid(rotated_images, nrow=2)
-        return grid
-    
-    def validation_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self.model(x)
-        loss = self.mse(y_hat, y)
-        self.log("val_loss", loss, sync_dist=True, prog_bar=True)
-        self.log("val_mae", self.mae(y_hat, y), sync_dist=True, prog_bar=True)
-        return loss
+    def _validate(self, epoch, data_loader):
+        data_loader.sampler.set_epoch(epoch)
+        self.model.eval()
+        running_loss = 0.0
+        for idx, batch in enumerate(data_loader):
+            batch_loss = self._eval_step(batch, idx)
+            running_loss += batch_loss
 
-    def test_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self.model(x)
-        loss = self.mse(y_hat, y)
-        self.log("test_loss", loss, sync_dist=True, prog_bar=True)
-        self.log("test_mae", self.mae(y_hat, y), sync_dist=True, prog_bar=True)
-        return loss
+        average_loss = running_loss / len(data_loader)
+        print(f"GPU {self.global_rank} EPOCH {self.epochs_run} | Val Loss: {average_loss:.5f}")
+        return average_loss
 
-    def predict_step(self, batch, batch_idx):
-        x, y = batch
-        pred = self.model(x)
-        return pred
-
-    def configure_optimizers(self):
-        optimizer = optim.Adam(self.model.parameters(), lr=self.hparams.learning_rate, weight_decay=self.hparams.weight_decay)
-        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
-        return [optimizer], [scheduler]
+    def fit(self):
+        for epoch in range(self.epochs_run, self.config.max_epochs):
+            self._train(epoch, self.train_loader)
+            if self.val_loader:
+                val_loss = self._validate(epoch, self.val_loader)
+                # TODO: make sure everything is communicated properly. All reduce?
+                # TODO: add save snapshot for rank 0 only and for periodic snapshots
+                # TODO: add early stopping
+                if val_loss < self.best_loss:
+                    self.best_loss = val_loss
+                    self.best_epoch = epoch
+                    self._save_snapshot(self.config.snapshot_path)
+            self.epochs_run += 1
 
 
-def setup_data_loaders(annotations_file, img_dir, batch_size, num_workers, prefetch_factor, train_transform, val_transform, test_transform):
+def mse_loss(y_pred, y_true, scale=1):
+    return torch.mean((y_pred - y_true) ** 2) * scale
+
+
+def mae_loss(y_pred, y_true, scale=1):
+    return torch.mean(torch.abs(y_pred - y_true)) * scale
+
+
+def setup_optimizer(model, learning_rate, weight_decay):
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    return optimizer
+
+
+def setup_scheduler(optimizer, step_size, gamma):
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
+    return scheduler
+
+
+def setup_criterion():
+    criterion = mse_loss
+    return criterion
+
+
+def setup_data_loaders(data_config, transform=None, target_transform=None):
     train_dataset = RotatedImageDataset(
-        annotations_file,
-        img_dir,
-        subset="train",
-        transform=train_transform,
+        data_config, subset="train", transform=transform, target_transform=target_transform
     )
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=num_workers,
-        prefetch_factor=prefetch_factor,
-    )
-
+    val_dataset = RotatedImageDataset(data_config, subset="val", transform=transform, target_transform=target_transform)
     test_dataset = RotatedImageDataset(
-        annotations_file,
-        img_dir,
-        subset="test",
-        transform=test_transform,
-    )
-    test_loader = DataLoader(
-        test_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=num_workers,
-        prefetch_factor=prefetch_factor,
+        data_config, subset="test", transform=transform, target_transform=target_transform
     )
 
-    val_dataset = RotatedImageDataset(
-        annotations_file,
-        img_dir,
-        subset="val",
-        transform=val_transform,
-    )
-    val_loader = DataLoader(
-        val_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=num_workers,
-        prefetch_factor=prefetch_factor,
-    )
-    return train_loader, val_loader, test_loader
+    return train_dataset, val_dataset, test_dataset
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Train a model to correct image rotation.")
-    parser.add_argument("--batch_size", "-b", type=int, default=48, help="input batch size")
-    parser.add_argument("--num_epochs", "-e", type=int, default=25, help="number of epochs to train for")
-    parser.add_argument("--num_workers", "-w", type=int, default=12, help="number of data loading workers")
-    parser.add_argument("--gpus", "-g", type=int, default=2, help="number of GPUs to allocate per node")
-    parser.add_argument("--num_nodes", "-n", type=int, default=1, help="number of nodes to use")
-    parser.add_argument("--img_dir", type=str, required=True, help="directory with images")
-    parser.add_argument("--annotations_file", type=str, required=True, help="annotations CSV file")
-    parser.add_argument("--model", "-m", type=str, required=True, help="model to train")
+    parser = argparse.ArgumentParser(description="PyTorch SkewNet Training")
+    parser.add_argument("config", metavar="FILE", help="path to config file")
     return parser.parse_args()
 
 
+def setup_config(config):
+    train_config = TrainConfig(**config["train_config"])
+    optimizer_config = OptimizerConfig(**config["optimizer_config"])
+    scheduler_config = SchedulerConfig(**config["scheduler_config"])
+    data_config = DataConfig(**config["data_config"])
+    return train_config, optimizer_config, scheduler_config, data_config
+
+
+def get_train_objects(model, optimizer_config, scheduler_config, data_config):
+    model = ModelRegistry.get_model(model)
+    criterion = setup_criterion()
+    optimizer = setup_optimizer(model, optimizer_config.learning_rate, optimizer_config.weight_decay)
+    scheduler = setup_scheduler(optimizer, scheduler_config.step_size, scheduler_config.gamma)
+    train_dataset, val_dataset, _ = setup_data_loaders(data_config)
+    return model, criterion, optimizer, scheduler, train_dataset, val_dataset
+
+
+def ddp_setup():
+    init_process_group(backend="nccl")
+    torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
+
+
 def main():
+    ddp_setup()
+
     args = parse_args()
+    with open(args.config, "r") as f:
+        config = json.load(f)
 
-    LEARNING_RATE = 0.005
-    WEIGHT_DECAY = 0
-    BATCH_SIZE = args.batch_size
-    NUM_EPOCHS = args.num_epochs
-    NUM_WORKERS = args.num_workers
-    ALLOCATED_GPUS_PER_NODE = args.gpus
-    NUM_NODES = args.num_nodes
-    IMG_DIR = args.img_dir
-    ANNOTATIONS_FILE = args.annotations_file
-    MODEL = args.model
-    
+    train_config, optimizer_config, scheduler_config, data_config = setup_config(config)
 
-    # TODO: test the impact of using float16 instead of float32
-    torch.set_float32_matmul_precision("medium")
-
-    model = ModelRegistry.get_model(MODEL)
-    model = LightningRotationNet(model, LEARNING_RATE, WEIGHT_DECAY)
-
-    train_transform = transforms.Compose([transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),])
-    test_transform = transforms.Compose([])
-    val_transform = transforms.Compose([])
-
-    trainer = pl.Trainer(
-        accelerator="gpu",
-        strategy="ddp",
-        devices=ALLOCATED_GPUS_PER_NODE,
-        num_nodes=NUM_NODES,
-        max_epochs=NUM_EPOCHS,
-        logger=pl.loggers.TensorBoardLogger("logs/tensorboard", name=f"SkewNet-{MODEL}"),
-        callbacks=[
-            pl.callbacks.LearningRateMonitor(logging_interval="step"),
-            pl.callbacks.ModelCheckpoint(
-                dirpath="/scratch/gpfs/RUSTOW/deskewing_models",
-                filename=f"SkewNet-{MODEL}-" + "{epoch:02d}-{val_loss:.2f}",
-                monitor="val_loss",
-            )
-        ],
+    model, criterion, optimizer, scheduler, train_dataset, val_dataset = get_train_objects(
+        config["model"], optimizer_config, scheduler_config, data_config
     )
 
-    
-    train_loader, val_loader, test_loader = setup_data_loaders(
-        ANNOTATIONS_FILE,
-        IMG_DIR,
-        BATCH_SIZE,
-        NUM_WORKERS,
-        prefetch_factor=1,
-        train_transform=train_transform,
-        val_transform=val_transform,
-        test_transform=test_transform,
-    )
+    trainer = Trainer(train_config, model, criterion, optimizer, scheduler, train_dataset, val_dataset)
+    trainer.fit()
 
-
-    trainer.fit(model, train_loader, val_loader)
-    trainer.test(model, test_loader)
+    destroy_process_group()
 
 
 if __name__ == "__main__":
