c61d869cbb5e302870150b994d99657de554a3bc
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2024-01-08 14:20:32 -0500

    Renaming to RotationNet

--- src/SkewNet/utils/combining_annotation_data.py
+++ src/SkewNet/utils/combining_annotation_data.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from SkewNet.utils.image_utils import rotate_image
+from RotationNet.utils.image_utils import rotate_image
 
 
 # %%
